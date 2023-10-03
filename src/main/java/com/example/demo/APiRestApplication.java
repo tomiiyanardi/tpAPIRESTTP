bc5c068745eecc12998a4a1eab9969d2790e25d1
tomiiyanardi <tomasyanardi@gmail.com>
2023-10-03 13:48:13 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/APiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APiRestApplication.class, args);
+			System.out.println("funciono");
+}
+}
